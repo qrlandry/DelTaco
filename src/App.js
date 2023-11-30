2aa298fdb2d3e2b20e2ecf31a36c4bb4f4927820
qrlandry <qrlandry@gmail.com>
2023-11-29 23:39:07 -0500

    style it next

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/nav";
+import Home from "./components/home"
+import Menu from "./components/menu";
+import Contact from "./components/contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Home />
+      <Menu/>
+      <Contact/>
     </div>
   );
 }
